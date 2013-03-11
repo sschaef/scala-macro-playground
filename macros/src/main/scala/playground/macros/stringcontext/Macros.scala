dca5bc5d7b889bd7d3d06eac94eb79d38136ac90
Simon Schaefer <mail@antoras.de>
2013-03-11 21:50:06 +0100

    refactoring in StringContext macro

--- macros/src/main/scala/playground/macros/stringcontext/Macros.scala
+++ macros/src/main/scala/playground/macros/stringcontext/Macros.scala
@@ -5,11 +5,12 @@ import scala.reflect.macros.Macro
 
 object Macros {
 
+  // http://stackoverflow.com/questions/15329027/string-interpolation-and-macro-how-to-get-the-stringcontext-and-expression-loca
   sealed trait Piece
   case class Place(str: String) extends Piece
   case class Name(str: String) extends Piece
   case class Pos(column: Int, line: Int)
-  case class LocatedPieces(located: List[(String, Piece, Pos)])
+  case class LocatedPieces(located: Seq[(String, Piece, Pos)])
 
   implicit class s2pieces(sc: StringContext) {
     def s2(pieces: Piece*) = macro Impl.s2impl
@@ -21,72 +22,22 @@ trait Impl extends Macro {
   import Macros._
   import c.universe.{ Name => _, _ }
 
-  // pieces contain all the Piece instances passed inside of the string interpolation
   def s2impl(pieces: c.Expr[Piece]*): c.Expr[LocatedPieces] = {
-
-    c.prefix.tree match {
-      // access data of string interpolation
+    val parts = c.prefix.tree match {
       case Apply(_, List(Apply(_, rawParts))) =>
-
-        // `parts` contain the strings a string interpolation is built of
-        val parts = rawParts map { case Literal(Constant(const: String)) => const }
-        // translate compiler positions to a data structure that can live outside of the compiler
-        val positions = pieces.toList map (_.tree.pos) map (p => Pos(p.column, p.line))
-        // discard last element of parts, `transpose` does not work otherwise
-        // trim parts to discard unnecessary white space
-        val data = List(parts.init map (_.trim), pieces.toList, positions).transpose
-        // create an AST containing a List[(String, Piece, Pos)]
-        val tupleAST = data map { case List(part: String, piece: c.Expr[_], Pos(column, line)) =>
-          toTupleAST(toAST(part), piece.tree, toPosAST(toAST(column), toAST(line)))
-        }
-        // create an AST of `LocatedPieces`
-        val locatedPiecesAST = toLocatedPiecesAST(toListAST(tupleAST))
-        c.Expr(locatedPiecesAST)
-
-      case _ =>
-        c.abort(c.enclosingPosition, "invalid")
+        rawParts zip (pieces map (_.tree)) map {
+          case (Literal(Constant(rawPart: String)), piece) =>
+            val line = c.literal(piece.pos.line).tree
+            val column = c.literal(piece.pos.column).tree
+            val part = c.literal(rawPart.trim).tree
+            toAST[(_, _, _)](part, piece, toAST[Pos](line, column))
+      }
     }
+    c.Expr(toAST[LocatedPieces](toAST[Seq[_]](parts: _*)))
   }
 
-  // helper methods
-  def typeIdent[A : TypeTag] =
-    Ident(typeTag[A].tpe.typeSymbol)
-
-  def companionIdent[A : TypeTag] =
-    Ident(typeTag[A].tpe.typeSymbol.companionSymbol)
-
-  def identFromString(tpt: String) =
-    Ident(c.mirror.staticModule(tpt))
-
-  // We need to translate the data calculated inside of the macro to an AST
-  // in order to write it back to the compiler.
-  def toAST(any: Any) =
-    Literal(Constant(any))
-
-  def toPosAST(column: Tree, line: Tree) =
+  def toAST[A : TypeTag](xs: Tree*): Tree =
     Apply(
-      Select(companionIdent[Pos], TermName("apply")),
-      List(column, line))
-
-  def toTupleAST(t1: Tree, t2: Tree, t3: Tree) =
-    Apply(
-      TypeApply(
-        Select(identFromString("scala.Tuple3"), TermName("apply")),
-        List(typeIdent[String], typeIdent[Piece], typeIdent[Pos])),
-      List(t1, t2, t3))
-
-  def toLocatedPiecesAST(located: Tree) =
-    Apply(
-      Select(companionIdent[LocatedPieces], TermName("apply")),
-      List(located))
-
-  def toListAST(xs: List[Tree]) =
-    Apply(
-      TypeApply(
-        Select(identFromString("scala.collection.immutable.List"), TermName("apply")),
-        List(AppliedTypeTree(
-          typeIdent[Tuple3[String, Piece, Pos]],
-          //identFromString("scala.Tuple3"),
-          List(typeIdent[String], typeIdent[Piece], typeIdent[Pos])))),
-      xs)
+      Select(Ident(typeOf[A].typeSymbol.companionSymbol), TermName("apply")),
+      xs.toList)
 }
