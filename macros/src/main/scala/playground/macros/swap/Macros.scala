34dba82635ab8885210bb9015cd8826356821f8f
Simon Schaefer <mail@antoras.de>
2013-03-11 22:39:34 +0100

    swap macro update

--- macros/src/main/scala/playground/macros/swap/Macros.scala
+++ macros/src/main/scala/playground/macros/swap/Macros.scala
@@ -6,23 +6,23 @@ import scala.reflect.macros.Macro
 object Macros {
 
   // http://weblogs.java.net/blog/cayhorstmann/archive/2013/01/14/first-look-scala-macros
-  def swap(a: Any, b: Any): Unit = macro Impl.swap
+  def swap[A](a: A, b: A): Unit = macro Impl.swap[A]
 }
 
 trait Impl extends Macro {
 
-  import c.universe._, Flag._
+  import c.universe._
 
-  def swap(a: c.Expr[Any], b: c.Expr[Any]): c.Expr[Unit] = {
+  def swap[A](a: c.Expr[A], b: c.Expr[A]): c.Expr[Unit] = {
     c.Expr(Block(
       List(
-        ValDef(Modifiers(MUTABLE), TermName("$temp"), TypeTree(), a.tree),
+        ValDef(NoMods, TermName("$temp"), TypeTree(), a.tree),
         assign(a, b),
-        assign(b, c.Expr[Any](Ident(TermName("$temp"))))),
-      Literal(Constant(()))))
+        assign(b, c.Expr(Ident(TermName("$temp"))))),
+      c.literalUnit.tree))
   }
 
-  def assign(a: c.Expr[Any], b: c.Expr[Any]) = a.tree match {
+  def assign[A](a: c.Expr[A], b: c.Expr[A]) = a.tree match {
     case ident: Ident =>
       Assign(ident, b.tree)
 
